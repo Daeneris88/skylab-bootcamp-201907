3f113b45f9276ab2c9658959f4231ab4e4863bf0
Anna <daeneris88@gmail.com>
2019-07-31 12:48:20 +0200

    app

---
+++ staff/anna-nolla/e-duck/1/components/login.jsx
@@ -0,0 +1,26 @@
+function Login(props){
+
+    return <><form onSubmit = { event => {
+        event.preventDefault()
+
+        const { target: { 
+            email: { value: email } ,
+            password: { value: password }       
+        } } = event
+
+        props.onLogin(email, password)
+    }}>
+        <label htmlFor="email">E-mail</label>
+        <input type="email" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <button>Submit</button>
+    </form>
+
+    <a href="" onClick={ event => {
+        event.preventDefault()
+
+        props.onBack()
+    } } >Back</a>
+    </>
+}
